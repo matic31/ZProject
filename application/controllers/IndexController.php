69b1334faea92726e832a2fee9db1ad71badda8f
matic31 <maticnk@gmail.com>
2015-11-29 19:09:49 +0100

    setting up project

---
+++ application/controllers/IndexController.php
@@ -0,0 +1,22 @@
+<?php
+
+class IndexController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+       //require_once 'Zend/Loader.php';
+        /*$client = new Zend_Soap_Server('servis/manage');
+        $cons = $client->getConcerts();
+        print_r($cons);*/
+        //echo 'pera';
+    }
+
+
+}
+
