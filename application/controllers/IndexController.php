0162fae76b0c72837cd5fbf5e5139a0027a8c15d
matic31 <maticnk@gmail.com>
2015-11-29 22:45:52 +0100

    fixed SOAP Client

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -10,10 +10,16 @@ class IndexController extends Zend_Controller_Action
 
     public function indexAction()
     {
-       //require_once 'Zend/Loader.php';
-        /*$client = new Zend_Soap_Server('servis/manage');
+        require_once 'Zend/Loader.php';
+        Zend_Loader::loadClass('Zend_Soap_Client');
+        $options = array(
+        'location' => 'http://servis/Index/manage',
+        'uri' => 'http://servis/Index/manage'
+        );
+        $client = new Zend_Soap_Client(null, $options); 
         $cons = $client->getConcerts();
-        print_r($cons);*/
+       // echo $cons[0]->getTime();
+        print_r($cons);
         //echo 'pera';
     }
 
